119baf6dbfaf012670442e81cd266f9d06f323cd
hacraven <hacraven1@gmail.com>
2018-07-02 10:20:35 -0400

    added tags and model associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_02_023345) do
+ActiveRecord::Schema.define(version: 2018_07_02_141401) do
+
+  create_table "tags", force: :cascade do |t|
+    t.string "phrase"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tweet_tags", force: :cascade do |t|
+    t.integer "tweet_id"
+    t.integer "tag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tweets", force: :cascade do |t|
     t.string "message"
